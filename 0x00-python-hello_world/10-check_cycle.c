01fbde88af6d4a02b96adeb2d2c407dac335345b
Velma4009 <velmaatieno1996@gmail.com>
2023-11-27 11:33:15 -0800

    actually C programming is not my favourite

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,32 @@
 #include "lists.h"
-#include <stdlib.h>
 /**
- *check_cycle-checks the presence of cycle
- *@list: pointer to listint_t
- *Return: int
+ * check_cycle - cycle tortoise and hare
+ * @list: pointer to head
+ * Return: 1 on success, 0 otherwise.
  */
 int check_cycle(listint_t *list)
 {
-listint_t *jump, *node;
+	listint_t *tortoise;
+	listint_t *hare;
 
-if (!list || !list->next)
-return (0);
-
-node = list;
-jump = list->next;
-
-while (jump && jump->next && node && node->next)
-{
-if (jump == node)
-return (1);
-jump = jump->next->next;
-if (!jump)
-
-break;
-node = node->next;
-}
-return (0);
+	if (list == NULL)
+		return (0);
+	tortoise = list;
+	hare = list;
+	while (hare->next != NULL && hare->next->next != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+		{
+			tortoise = list;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (1);
+		}
+	}
+	return (0);
 }
