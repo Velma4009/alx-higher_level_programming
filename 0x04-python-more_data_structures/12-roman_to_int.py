439f5390a7679b99855fe8b48d396ab7fcbfac2e
Velma4009 <velmaatieno1996@gmail.com>
2023-12-06 06:31:37 -0800

    file updated

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,5 @@
-#!/usr/bin/pthon3
+#!/usr/bin/python3
+
 
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
@@ -23,7 +24,7 @@ def roman_to_int(roman_string):
 
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+            num += roman_dict[roman_string[i]] * -1
 
         else:
             num += roman_dict[roman_string[i]]
