dd9a28e75c5b0f58e774e29e5f03df8e9cb3a42d
Velma4009 <velmaatieno1996@gmail.com>
2024-04-20 15:58:29 -0700

    python-network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
-# send a GET request to a URL with curl, with a custom header variable
-curl -s -H "X-School-User-Id: 98" "$1"
+# sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
