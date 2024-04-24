5ed172c10227992faeafbf3e95920dec5d39fc1e
Velma4009 <velmaatieno1996@gmail.com>
2024-04-23 19:18:31 -0700

    web_scraping_project

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,14 @@
-
 #!/usr/bin/node
-const fs = require('fs');
+// a script to get the contents of a web page
+
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const fs = require('fs');
+const filePath = process.argv[3];
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  fs.writeFile(filePath, body, err => {
+    if (err) console.error(err);
+  });
+});
