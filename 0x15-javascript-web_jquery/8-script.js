73ff4995a60cbb5d310b95472d14465c778d8187
Velma4009 <velmaatieno1996@gmail.com>
2024-05-02 19:51:54 -0700

    0x15-javascript-web_jquery_project

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(() => {
+    $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+      $.each(data.results, function (i, movie) {
+        $('#list_movies').append('<li>' + movie.title + '</li>');
+      });
+    });
+  });
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,4 @@
+This directory contains the concepts and
+code implementation of javascript 
+web-query. The tasks were provided by
+ALX Africa SE Program
